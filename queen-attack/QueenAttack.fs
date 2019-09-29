b72ab728724747af2b64561c3975231c22e1c76b
Victor Grigoriu <grigoriu@adobe.com>
2019-09-29 12:50:33 +0300

    test 1

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,5 +1,5 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+let create (position: int * int) = true
 
 let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
