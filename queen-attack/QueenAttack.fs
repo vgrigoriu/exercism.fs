deee888fb31602c07a518e37255e48f2d173ad4f
Victor Grigoriu <grigoriu@adobe.com>
2019-09-29 14:19:26 +0300

    Add empty line

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -7,6 +7,7 @@ let create (position : int * int) =
 let canAttack (queen1 : int * int) (queen2 : int * int) =
     let row1, column1 = queen1
     let row2, column2 = queen2
+
     row1 = row2
     || column1 = column2
     || abs (row1 - row2) = abs (column2 - column1)
