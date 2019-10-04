324dec94ed0c083a03f621c3b17526059fe5f373
Victor Grigoriu <grigoriu@adobe.com>
2019-10-04 19:59:52 +0300

    all tests

--- accumulate/AccumulateTest.fs
+++ accumulate/AccumulateTest.fs
@@ -21,17 +21,17 @@ let ``Identity accumulation returns unmodified list`` () =
 let ``Accumulate squares`` () =
     accumulate (fun x -> x * x) [1; 2; 3] |> should equal [1; 4; 9]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Accumulate upcases`` () =
     accumulate (fun (x:string) -> x.ToUpper()) ["hello"; "world"]
     |> should equal ["HELLO"; "WORLD"]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Accumulate reversed strings`` () =
     accumulate reverse (List.ofArray ("the quick brown fox etc".Split(' ')))
     |> should equal (List.ofArray("eht kciuq nworb xof cte".Split(' ')))
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Accumulate within accumulate`` () =
     accumulate (fun (x:string) -> String.concat " " (accumulate (fun y -> x + y) ["1"; "2"; "3"])) ["a"; "b"; "c"]
     |> should equal ["a1 a2 a3"; "b1 b2 b3"; "c1 c2 c3"]
