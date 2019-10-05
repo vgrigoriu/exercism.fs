ae517d654af3f50ddc02e547dfabc84f12ab268f
Victor Grigoriu <grigoriu@adobe.com>
2019-10-05 21:28:09 +0300

    Compile

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,5 +1,14 @@
 ﻿module SpaceAge
 
 // TODO: define the Planet type
+type Planet =
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
 
 let age (planet: Planet) (seconds: int64): float = failwith "You need to implement this function."
