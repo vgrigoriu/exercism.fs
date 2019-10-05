40b0183b2fc0d8a2db5d3cc59f2b543e4d981e49
Victor Grigoriu <grigoriu@adobe.com>
2019-10-05 21:44:23 +0300

    Refactor

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -12,15 +12,16 @@ type Planet =
 
 let private earthYear = 365.25 * 24.0 * 3600.0
 
-let rec private secondsInYear(planet: Planet): float =
+let rec private periodInEarthYears(planet: Planet): float =
     match planet with
-        | Earth -> 1.0 * earthYear
-        | Mercury -> 0.2408467 * earthYear
-        | Venus -> 0.61519726 * earthYear
-        | Mars -> 1.8808158 * earthYear
-        | Jupiter -> 11.862615 * earthYear
-        | Saturn -> 29.447498 * earthYear
-        | Uranus -> 84.016846 * earthYear
-        | Neptune -> 164.79132 * earthYear
+        | Earth -> 1.0
+        | Mercury -> 0.2408467
+        | Venus -> 0.61519726
+        | Mars -> 1.8808158
+        | Jupiter -> 11.862615
+        | Saturn -> 29.447498
+        | Uranus -> 84.016846
+        | Neptune -> 164.79132
 
-let age (planet: Planet) (seconds: int64): float = (float seconds) / (secondsInYear planet)
+let age (planet: Planet) (seconds: int64): float =
+    (float seconds) / earthYear / periodInEarthYears planet
