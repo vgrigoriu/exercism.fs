ae9ecd96af23e7026067eb807c02d1f17ecaf4d6
Victor Grigoriu <grigoriu@adobe.com>
2019-10-05 21:41:41 +0300

    all tests

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -1,6 +1,5 @@
 ﻿module SpaceAge
 
-// TODO: define the Planet type
 type Planet =
     | Earth
     | Mercury
@@ -11,9 +10,17 @@ type Planet =
     | Uranus
     | Neptune
 
+let private earthYear = 365.25 * 24.0 * 3600.0
+
 let rec private secondsInYear(planet: Planet): float =
     match planet with
-        | Earth -> 365.25 * 24.0 * 3600.0
-        | Mercury -> 0.2408467 * secondsInYear Earth
+        | Earth -> 1.0 * earthYear
+        | Mercury -> 0.2408467 * earthYear
+        | Venus -> 0.61519726 * earthYear
+        | Mars -> 1.8808158 * earthYear
+        | Jupiter -> 11.862615 * earthYear
+        | Saturn -> 29.447498 * earthYear
+        | Uranus -> 84.016846 * earthYear
+        | Neptune -> 164.79132 * earthYear
 
 let age (planet: Planet) (seconds: int64): float = (float seconds) / (secondsInYear planet)
--- space-age/SpaceAgeTest.fs
+++ space-age/SpaceAgeTest.fs
@@ -15,27 +15,27 @@ let ``Age on Earth`` () =
 let ``Age on Mercury`` () =
     age Mercury 2134835688L |> should (equalWithin 0.01) 280.88
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Venus`` () =
     age Venus 189839836L |> should (equalWithin 0.01) 9.78
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Mars`` () =
     age Mars 2129871239L |> should (equalWithin 0.01) 35.88
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Jupiter`` () =
     age Jupiter 901876382L |> should (equalWithin 0.01) 2.41
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Saturn`` () =
     age Saturn 2000000000L |> should (equalWithin 0.01) 2.15
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Uranus`` () =
     age Uranus 1210123456L |> should (equalWithin 0.01) 0.46
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Age on Neptune`` () =
     age Neptune 1821023456L |> should (equalWithin 0.01) 0.35
 
