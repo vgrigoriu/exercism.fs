f0ff5c00361cee2731831a2bcb7d18cc85f7da7b
Victor Grigoriu <grigoriu@adobe.com>
2019-10-06 13:31:49 +0300

    test 1

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -2,10 +2,14 @@
 
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School =
+    let students = school.TryFind(grade) |> Option.defaultValue []
+    let newStudents = student :: students
+    Map.add grade newStudents school
 
-let roster (school: School): string list = failwith "You need to implement this function."
+let roster (school: School): string list =
+    school |> Seq.collect (fun kv -> kv.Value) |> List.ofSeq
 
 let grade (number: int) (school: School): string list = failwith "You need to implement this function."
