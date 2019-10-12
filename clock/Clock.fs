7dc8efa85e58e484607f20828c9a8c2a70fde9c2
Victor Grigoriu <grigoriu@adobe.com>
2019-10-12 10:11:50 +0300

    missing '

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -7,7 +7,7 @@ let private modulo n m = ((n % m) + m) % m
 let create hours minutes =
     let minutes' = modulo minutes 60
     let hours' = hours + (minutes - minutes') / 60
-    { hours = modulo hours' 24; minutes = minutes }
+    { hours = modulo hours' 24; minutes = minutes' }
 
 let add minutes (clock: Clock) = create clock.hours (clock.minutes + minutes)
 
