ae22c7a06a939faf0deb44e94dede0f5ddb50865
Victor Grigoriu <grigoriu@adobe.com>
2019-10-20 20:12:25 +0300

    test 2

--- bob/Bob.fs
+++ bob/Bob.fs
@@ -1,3 +1,8 @@
 ﻿module Bob
 
-let response (input: string): string = "Whatever."
+let response (input: string): string =
+    let isShout = input |> Seq.filter System.Char.IsLetter |> Seq.forall System.Char.IsUpper
+    if (isShout) then
+        "Whoa, chill out!"
+    else
+        "Whatever."
